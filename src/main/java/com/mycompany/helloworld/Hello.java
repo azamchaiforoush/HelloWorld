5a858ba853a8fbf1ab4330b98413263334d8a031
azamchaiforoush <84159966+azamchaiforoush@users.noreply.github.com>
2021-06-06 00:34:31 -0700

    HelloWorld in NetBeans

---
+++ src/main/java/com/mycompany/helloworld/Hello.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.helloworld;
+
+/**
+ *
+ * @author azamchaiforoush
+ * email  azam.chaifroosh2006@gmail.com
+ * date  may,30,2021
+ * purpose my first Hello World program is an IDE
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
+    
+
